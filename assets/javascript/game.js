85d9203e900513ab22bcdb200bd49d0b987585e0
Bigshmow <dstewart88@gmail.com>
2019-07-09 22:07:48 -0700

    brainstormed variables for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// alert("js link worked!!");
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var blanks = 0;
+
+var wordguess = ["rick","morty","beth","squanchy","jerry","wubalubbadubdub","sanchez","szechuan"];
+
+var pcword = [""];
+
+var lastguess = [];
+
+
+
+
